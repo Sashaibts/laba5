09daf0e41035e380e6a88fb37241041bb84026b1
Sashaibts <ssavina154@gmail.com>
2025-04-28 02:14:29 +0500

    Создание

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stdio.h>
+#include <stdlib.h>
+#include "PatternTemplates (2).h"
+
+using namespace std;
+
+enum class SportDanger : int
+{
+    Safely,
+    Medium,
+    Dangerous,
+    Unknown
+};
+int main()
+{
+    return 0;
+}
